f0577c73c2f15162f187f53e9dbc990c1590643c
1933376997@qq.com <1933376997@qq.com>
2023-04-12 20:50:42 +0800

    mian分支代码

--- DevGitBranch.java
+++
@@ -1,5 +0,0 @@
-public class DevGitBranch {
-    public static void main(String[] args) {
-        System.out.println("这是一个dev分支的代码......");
-    }
-}
---
+++ MainGitBranch.java
@@ -0,0 +1,5 @@
+public class MainGitBranch {
+    public static void main(String[] args) {
+        System.out.println("这是一个main分支的代码......");
+    }
+}
